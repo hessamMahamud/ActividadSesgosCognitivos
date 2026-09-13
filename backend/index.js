117f967d2012d7527437eaefbd8536fea3283501
Hessam <h_mahamud_b@hotmail.com>
2026-09-13 17:35:49 -0500

    chore: reorder structure for vercel

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-import { app } from './src/server.js';
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
--- backend/src/routes/reportes.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { postReporte, getReportes } from '../controllers/reportes.controller.js';
-
-export const reportesRouter = Router();
-
-reportesRouter.post('/', postReporte);
-reportesRouter.get('/', getReportes);
